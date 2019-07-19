076a3feb3ad52ef7db34af79c4b36ed3f3be814a
James Lane <jameslane93@gmail.com>
2019-07-19 15:17:44 -0400

    Adds interaction matrix derivation

--- ao_code/shwfs/make_cm.m
+++ ao_code/shwfs/make_cm.m
@@ -9,10 +9,41 @@ nAverage = 5;         % Number of frames to average for each push-pull
 nbPushPull = 5;       % Number of push-pull
 pushPullValue = 0.12; % Magnitude of push-pull
 
+% Setup the interaction matrix array
+nSlopeX = size(wfs.slopeX,1);
+nSlopeY = size(wfs.slopeY,1);
+nSlopes = nSlopeX + nSlopeY;
+interactionMatrix = zeros( dm.nAct, nSlopeX+nSlopeY );
 
-% Loop over the
+% Loop over the number of actuators
+for i=1:dm.nAct
+  
+  % Setup the array that will hold the push-pull samples
+  sDiff = zeros(nSlopes,1)
+  
+  % For each actuator loop over the number of push-pull pairs
+  for j=1:nbPushPull
+    
+    % Acquire slopes
+    dm.cmdVector(i)=pushPullValue;
+    pause(pauseTime)
+    sPushX = wfs.slopeX;
+    sPushY = wfs.slopeY;
+    dm.cmdVector(i)=-pushPullValue;
+    pause(pauseTime)
+    sPullX = wfs.slopeX;
+    sPullY = wfs.slopeY;
+    
+    % Calculate the difference and append
+    sDiffX = sPushX-sPullX;
+    sDiffY = sPushY-sPullY;
+    sDiff = sDiff + [sDiffX,sDiffY];
+    
+  end
+  
+  % Take the average of the slope vectors
+  sVec = sDiff / nbPushPull / pushPullValue
 
-
-
-
-%
+  % Append this column into the interaction matrix
+  interactionMatrix(i,:) = sVec;
+end
