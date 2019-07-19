9f80a2868ea84a331cf10c4a6ddce3291b084915
James Lane <jameslane93@gmail.com>
2019-07-19 12:27:05 -0400

    Begin code to run SHWFS

---
+++ ao_code/shwfs/make_cm.m
@@ -0,0 +1,18 @@
+% make_cm.m
+% Author - James Lane
+% Make the command matrix for the Shack Hartmann Wavefront Sensor
+
+% Define how many pushes and pulls there will be, and what their magnitudes
+% will be.
+pauseTime = 0.1;      % Time to pause between command and slope acquisition
+nAverage = 5;         % Number of frames to average for each push-pull
+nbPushPull = 5;       % Number of push-pull
+pushPullValue = 0.12; % Magnitude of push-pull
+
+
+% Loop over the
+
+
+
+
+%
